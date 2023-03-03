9b5889a012ceda6cec8246bb7911ea4e4de573ed
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2023-03-03 16:20:30 +0530

    routing added to react app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Route } from "react-router-dom";
+
 import MainDiv from "./components/MainDiv";
 import MainNavbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
 
 function App(){
     return (
         <div>
-            <MainNavbar/>
-            <MainDiv/>
+            <Route path="/">
+                <MainNavbar/>
+                <MainDiv/>
+            </Route>
+            <Route path="/signup">
+                <SignUp/>
+            </Route>
         </div>
     );
 }
