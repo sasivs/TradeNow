382e135c71599876d1a780cd1397ac929c2602a0
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2023-03-11 18:12:34 +0530

    useInput custom hook added

---
+++ src/customHooks/useInput.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const useInput = (validator) => {
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isTouched, setIsTouched] = useState(false);
+
+    const {isValid, message} = validator(enteredValue);
+
+    const hasError = !isValid && isTouched;
+
+    const inputChangeHanlder = (event) => {
+        setEnteredValue(event.target.value);
+    }
+
+    const inputOnBlurHandler = () => {
+        setIsTouched(true);
+    }
+
+    return {
+        value: enteredValue, hasError, inputChangeHanlder, inputOnBlurHandler, message: message
+    }
+}
+
+
