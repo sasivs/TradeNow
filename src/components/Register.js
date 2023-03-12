bb3695c4e6025032252288902a72a8427aa3a59a
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2023-03-12 11:39:22 +0530

    register component updated

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 import "./register.css";
+import { useEffect, useState } from "react";
 
 function Register(props) {
   const validateEmail = (email) => {
@@ -11,15 +12,17 @@ function Register(props) {
   };
 
   const validatePasswd = (passwd) => {
-    const passwdRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$/;
+    const passwdRegex = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
+    );
     let message = "";
-    const isValid = passwdRegex.test(passwd);
+    let isValid = passwdRegex.test(passwd);
     if (!isValid) {
       message =
         "Password should contain atleast one lowercase, one uppercase, one numeric, one special characters.";
-    } else if (passwd.length() < 8) {
+    } else if (passwd.length < 8) {
       message = "Password should have a minimum length of 8.";
+      isValid = false;
     }
     return { isValid: isValid, message: message };
   };
@@ -27,6 +30,7 @@ function Register(props) {
   const {
     value: emailEnteredValue,
     hasError: emailInputHasError,
+    isValid: emailIsValid,
     inputChangeHanlder: emailOnChangeHandler,
     inputOnBlurHandler: emailOnBlurHandler,
   } = useInput(validateEmail);
@@ -34,68 +38,93 @@ function Register(props) {
   const {
     value: passwdEnteredValue,
     hasError: passwdHasError,
+    isValid: passwdIsValid,
     inputChangeHanlder: passwdOnChangeHandler,
     inputOnBlurHandler: passwdOnBlurHandler,
     message: passwdMessage,
   } = useInput(validatePasswd);
 
+  const [formIsValid, setFormIsValid] = useState(true);
+
+  const [formIsSubmitted, setFormIsSubmitted] = useState(false);
+
+  useEffect(()=>{
+    setFormIsValid(emailIsValid && passwdIsValid)
+    
+  }, [emailIsValid, passwdIsValid]);
+
   const history = useHistory();
   const navigate = (path) => history.push(path);
+  
   const onFormSubmit = (event) => {
     event.preventDefault();
-    axios
-      .post("http://localhost:3001/api/auth/emailverification", {
-        email: emailEnteredValue,
-      })
-      .then((res) => {
-        navigate("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    emailOnBlurHandler();
+    passwdOnBlurHandler();
+    setFormIsSubmitted(true);
+    if (formIsValid) {
+      console.log('Form is valid');
+      axios
+        .post("http://localhost:3001/api/auth/emailverification", {
+          email: emailEnteredValue,
+          password: passwdEnteredValue,
+        })
+        .then((res) => {
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log("Errr");
+    }
+    
   };
 
   return (
-      <div>
-        <div className="email-form">
-          <form onSubmit={onFormSubmit}>
-            <input
-              type={"email"}
-              placeholder={"Your Email address"}
-              className="email-input input"
-              onChange={emailOnChangeHandler}
-              onBlur={emailOnBlurHandler}
-              value={emailEnteredValue}
-            />
+    <div>
+      <p className="error-label">
+          {formIsSubmitted && !formIsValid && "Please clear all form errors."}
+      </p>
+      <div className="email-form">
+        <form onSubmit={onFormSubmit}>
+          <input
+            type={"email"}
+            placeholder={"Your Email address"}
+            className="email-input input"
+            onChange={emailOnChangeHandler}
+            onBlur={emailOnBlurHandler}
+            value={emailEnteredValue}
+          />
 
-            <p className="error-label">
-              {emailInputHasError && "Please enter valid Email address"}
-            </p>
-            <br />
-            <input
-              type={"password"}
-              placeholder={"Your password"}
-              className="password-input input"
-              onChange={passwdOnChangeHandler}
-              onBlur={passwdOnBlurHandler}
-              value={passwdEnteredValue}
-            />
-            <p className="error-label">
-              {passwdHasError && "Please enter valid password   "}
-              {passwdHasError && <span class="password-helper-container">
-                ⓘ
-                <span class="password-message message">{passwdMessage}</span>
-              </span>}
-            </p>
-            <br />
-            <input
-              type={"submit"}
-              value={"Continue"}
-              className="continue-btn btn"
-            />
-          </form>
-        </div>
-     </div>
+          <p className="error-label">
+            {emailInputHasError && "Please enter valid Email address"}
+          </p>
+          <br />
+          <input
+            type={"password"}
+            placeholder={"Your password"}
+            className="password-input input"
+            onChange={passwdOnChangeHandler}
+            onBlur={passwdOnBlurHandler}
+            value={passwdEnteredValue}
+          />
+          <p className="error-label">
+            {passwdHasError && "Please enter valid password   "}
+            {passwdHasError && (
+              <span class="password-helper-container">
+                ⓘ<span class="password-message message">{passwdMessage}</span>
+              </span>
+            )}
+          </p>
+          <br />
+          <input
+            type={"submit"}
+            value={"Continue"}
+            className="continue-btn btn"
+          />
+        </form>
+      </div>
+    </div>
   );
 }
 
