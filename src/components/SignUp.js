01a4066b6cad73047ebd0377281ee430f9ae8e36
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2023-03-25 23:51:47 +0530

    Signup component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,21 +1,26 @@
 import Register from "./Register";
 import RegLoginParent from "./RegLoginParent";
 
-function SignUp(props){
+function SignUp(props) {
+  props = {
+    email: {
+      side_head: ["Buy, Sell,", "Invest,", "Learn, Analyze,", "Repeat"],
+      title: "TradeNow",
+    },
+    childComponent: Register,
+    childTitle: "Register",
+  };
 
-    props = {
-        email:{
-            side_head: ["Buy, Sell,", "Invest,", "Learn, Analyze,", "Repeat"],
-            title: "TradeNow"
-        },
-        childComponent: Register
-    }
-
-    return (
-        <div className="signup-container">
-            <RegLoginParent side_head={props.email.side_head} title={props.email.title} childComponent={props.childComponent}/>
-        </div>
-    );
+  return (
+    <div className="signup-container">
+      <RegLoginParent
+        side_head={props.email.side_head}
+        title={props.email.title}
+        childComponent={props.childComponent}
+        childTitle={props.childTitle}
+      />
+    </div>
+  );
 }
 
 export default SignUp;
