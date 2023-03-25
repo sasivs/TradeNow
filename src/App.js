b03237ebe54441e547fa6b1bdeb6c3487a6239a9
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2023-03-25 23:54:46 +0530

    forgot and reset password routes added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route, Switch } from "react-router-dom";
 import MainDiv from "./components/MainDiv";
 import MainNavbar from "./components/Navbar";
 import SignUp from "./components/SignUp";
+import LoginPage from "./components/LoginPage";
+import ForgotPasswordPage from "./components/ForgotPasswordPage";
+import ResetPasswordPage from "./components/ResetPasswordPage";
 
 function App() {
   return (
@@ -14,6 +17,15 @@ function App() {
         </Route>
         <Route exact path="/signup">
           <SignUp />
+        </Route>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route exact path="/reset-password">
+          <ResetPasswordPage />
+        </Route>
+        <Route exact path="/forgot-password">
+          <ForgotPasswordPage />
         </Route>
       </Switch>
     </div>
